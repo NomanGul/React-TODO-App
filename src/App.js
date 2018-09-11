b853d8a757ca0124fb6b6bd21afbda8d3b52538b
Noman Gul <nomangul2001@gmail.com>
2018-09-12 01:21:24 +0500

    remove todo prop added

--- src/App.js
+++ src/App.js
@@ -61,6 +61,7 @@ class App extends Component {
                 todoContent={todo.todoContent}
                 todoId={todo.id}
                 key={todo.id}
+                removeTodo={this.removeTodo}
               />
             );
           })}
